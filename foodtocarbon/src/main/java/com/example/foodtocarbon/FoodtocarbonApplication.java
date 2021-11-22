e446591cf1f2fe5f2a8ce89b6ce8b5f61bcdf8a0
TeaKn <ttea.kknezevic@gmail.com>
2021-11-22 11:28:24 +0100

    adding foodtocarbon dir

---
+++ foodtocarbon/src/main/java/com/example/foodtocarbon/FoodtocarbonApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodtocarbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodtocarbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodtocarbonApplication.class, args);
+	}
+
+}
---
+++ foodtocarbon/src/test/java/com/example/foodtocarbon/FoodtocarbonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodtocarbon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodtocarbonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
